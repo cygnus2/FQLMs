a01d91c283b79fdc524d8344c5570cc70747873e
Lukas Rammelmueller <lukas.rammelmueller@gmail.com>
2020-10-13 10:18:43 +0200

    typo

--- python_gauss_lattice/gauss_lattice/le_hamiltonian_builder.py
+++ python_gauss_lattice/gauss_lattice/le_hamiltonian_builder.py
@@ -46,7 +46,7 @@ class LowEnergyHamiltonianBuilder(HamiltonianBuilder):
 
 
     def _cycle_plaquettes(self, state):
-        self._log(str(self.level) + " // " + state)
+        self._log(str(self.level) + " // " + str(state))
         states = set()
         for p in self.plaquettes:
             new_state, _ = self.apply_u_dagger(state, p, sign=False)
