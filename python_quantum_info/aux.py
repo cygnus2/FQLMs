ca8f1fe965328f33e9b47fdeac67c73657038dc9
ramelmueller <lukas.rammelmueller@gmail.com>
2021-04-14 15:11:23 +0200

    Cleanup.;

--- python_quantum_info/aux.py
+++ python_quantum_info/aux.py
@@ -6,7 +6,7 @@ sys.path.append('../python_data/')
 from ed_result_wrapper import EDResult
 
 
-def get_result(param, datadir, dump_dir='results_dump/'):
+def get_result(param, datadir, dump_dir='results_dump/', use_pickle=(True,True)):
     """ This either reads from a result from the HDF5 file, or a pickle dump
         from a previous run. If new results from a new run should be loade, the
         appropriate pickle dump must be cleared, otherwise it defaults to that.
@@ -16,10 +16,12 @@ def get_result(param, datadir, dump_dir='results_dump/'):
     result_file = f"{dump_dir}/result_dump{tag}.pickle"
 
     result = None
-    if os.path.isfile(result_file):
+    if os.path.isfile(result_file) and use_pickle[0]:
         result = pickle.load(open(result_file, 'rb'))
+        print("LOADED FROM PICKLE: ", result)
     else:
         result = EDResult(param, datadir=datadir)
-        pickle.dump(result, open(result_file, 'wb'))
+        if use_pickle[1]:
+            pickle.dump(result, open(result_file, 'wb'))
 
     return result, tag
